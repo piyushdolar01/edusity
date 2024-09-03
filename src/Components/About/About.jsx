06e33b2c900710c3469da3d7e2966e1d74852a9b
piyushdolar01 <dolarpiyush1@gmail.com>
2024-09-03 20:51:13 +0530

    first commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,12 +3,19 @@ import "./About.css";
 import about_img from "../../assets/about.png";
 import play_icon from "../../assets/play_icon.png";
 
-const About = ({setPlayState}) => {
+const About = ({ setPlayState }) => {
   return (
     <div className="about">
       <div className="about-left">
         <img src={about_img} alt="" className="about-img" />
-        <img src={play_icon} alt="" className="play-icon" onClick={()=>{setPlayState(true)}} />
+        <img
+          src={play_icon}
+          alt=""
+          className="play-icon"
+          onClick={() => {
+            setPlayState(true);
+          }}
+        />
       </div>
       <div className="about-right">
         <h3>ABOUT UNIVERSITY</h3>
@@ -23,9 +30,15 @@ const About = ({setPlayState}) => {
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
           temporibus, fugit sit minima doloribus excepturi itaque labore
           explicabo distinctio sapiente quo corporis voluptates praesentium
-          harum corrupti nemo porro dignissimos atque. 
+          harum corrupti nemo porro dignissimos atque.
         </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, magnam facilis? Pariatur necessitatibus, eos nisi impedit minima distinctio architecto laborum quo incidunt animi. Ratione quibusdam dicta, repudiandae eos delectus explicabo nesciunt incidunt nulla quo aspernatur.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+          magnam facilis? Pariatur necessitatibus, eos nisi impedit minima
+          distinctio architecto laborum quo incidunt animi. Ratione quibusdam
+          dicta, repudiandae eos delectus explicabo nesciunt incidunt nulla quo
+          aspernatur.
+        </p>
       </div>
     </div>
   );
